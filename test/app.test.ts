bb6055c98b6f61131eaebc9348ec2eb2fe8fd92f
hawezo <enzo@innocenzi.dev>
2019-10-04 16:12:58 +0200

    Added tests

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,7 +1,94 @@
-import TailwindTheming from '../src';
+import { ThemingPlugin, DefaultThemes } from '../src';
+import cssMatcher from 'jest-matcher-css';
+import postcss from 'postcss';
+import tailwindcss from 'tailwindcss';
+import { OpacityVariant } from '../src/Theming/OpacityVariant';
 
-it('renders colors', () => {
-  const theming = TailwindTheming();
-  
-  expect(theming.theme).toHaveProperty('colors');
+expect.extend({
+  toMatchCss: cssMatcher,
+});
+
+const generatePluginCss = async (plugin: ThemingPlugin, config?: any) => {
+  const result = await postcss(
+    tailwindcss({
+      ...{
+        theme: {
+          ...plugin.getTheme(),
+        },
+        corePlugins: false,
+        plugins: [plugin.getTailwind()],
+      },
+      ...config,
+    })
+  ).process('@tailwind base; @tailwind components;', {
+    from: undefined,
+  });
+  return result.css;
+};
+
+it('generates root theme', async () => {
+  const plugin = new ThemingPlugin({
+    default: { type: 'light', colors: [{ name: 'primary', outputFormat: 'rgb', value: '#fff', opacityVariants: [] }] },
+  });
+
+  const css = await generatePluginCss(plugin);
+
+  // @ts-ignore
+  expect(css).toMatchCss(`
+      :root {
+        --color-primary: 255,255,255;
+        --color-scheme: light
+      }
+  `);
+});
+
+it('generates multiple themes', async () => {
+  const plugin = new ThemingPlugin({
+    default: { type: 'light', colors: [{ name: 'primary', outputFormat: 'rgb', value: '#fff', opacityVariants: [] }] },
+    night: { type: 'dark', colors: [{ name: 'primary', outputFormat: 'rgb', value: '#000', opacityVariants: [] }] },
+  });
+
+  const css = await generatePluginCss(plugin);
+
+  // @ts-ignore
+  expect(css).toMatchCss(`
+    :root {
+      --color-primary: 255,255,255;
+      --color-scheme: light
+    }
+    .theme-night {
+      --color-primary: 0,0,0;
+      --color-scheme: dark
+    }
+  `);
+});
+
+it('generates variants', async () => {
+  const opacityVariants: OpacityVariant[] = [{ name: 'disabled', value: 0.6 }];
+
+  const plugin = new ThemingPlugin({
+    default: { type: 'light', colors: [{ name: 'primary', outputFormat: 'rgb', value: '#fff', opacityVariants }] },
+  });
+
+  const css = await generatePluginCss(plugin);
+
+  // @ts-ignore
+  expect(css).toMatchCss(`
+    :root {
+      --color-primary: 255,255,255;
+      --disabled: 0.6;
+      --color-scheme: light
+    }
+  `);
+});
+
+it('generates default theme', async () => {
+  const plugin = new ThemingPlugin(DefaultThemes);
+  const mustContain = Object.keys(plugin.getTheme().colors.colors);
+  const css = await generatePluginCss(plugin);
+
+  mustContain.forEach(color => {
+    // @ts-ignore
+    expect(css.includes(`--${plugin.pluginConfig.colorVariablePrefix}-${color}`)).toBeTruthy();
+  });
 });
