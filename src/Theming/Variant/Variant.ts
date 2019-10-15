844fbb77189931da16c45fe6269a81ebbda74304
Enzo Innocenzi <enzo.inno@gmail.com>
2019-10-15 20:23:11 +0200

    Fixed type circular reference

--- src/Theming/Variant/Variant.ts
+++ src/Theming/Variant/Variant.ts
@@ -1,11 +1,9 @@
-import { DEFAULT_THEME_NAME } from "../Theme/Theme";
-
-const DEFAULT_VARIANT_NAME = 'default';
+export const DEFAULT_VARIANT_NAME = 'default';
 
 export abstract class Variant {
   name: string;
 
   constructor(name?: string) {
-    this.name = name || DEFAULT_THEME_NAME;
+    this.name = name || DEFAULT_VARIANT_NAME;
   }
 }
