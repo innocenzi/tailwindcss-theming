6d7a6c895d2c47ddbd9ca67e098d9b305a9ed5ff
Haew <hi@haew.xyz>
2020-01-11 15:24:31 +0100

    Fixed tests related to fonts (addition of Inter)

--- test/tailwind.test.ts
+++ test/tailwind.test.ts
@@ -117,7 +117,7 @@ it('generates a tailwind configuration extension', async () => {
       --font-title: Roboto,"Segoe UI";
       --spacing-huge: 64px
     }
-    .font-sans { font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji" }
+    .font-sans { font-family: Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji" }
     .font-serif { font-family: Georgia, Cambria, "Times New Roman", Times, serif }
     .font-mono { font-family: Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace }
     .font-title { font-family: var(--font-title) }
