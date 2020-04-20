7602773acf35c5895bc24f1222c8485750560315
Haew <hi@haew.xyz>
2020-04-20 22:49:46 +0200

    refactor: use lodash/set instead of custom method

--- src/Theming/Generator/getExtendedConfiguration.ts
+++ src/Theming/Generator/getExtendedConfiguration.ts
@@ -1,15 +1,15 @@
 import { Theme } from '../..';
 import { Configuration } from '../Configuration';
 import { getDefaultTheme, getCustomPropertyVariableName } from './utils';
-import { set } from './get';
+import set from 'lodash/set';
 
 export function getExtendedConfiguration(themes: Theme[], config: Configuration) {
   const extendConfiguration: any = {};
   const properties = getDefaultTheme(themes)
     .getCustomProperties()
-    .filter(prop => prop.extends());
+    .filter((prop) => prop.extends());
 
-  properties.forEach(property => {
+  properties.forEach((property) => {
     set(extendConfiguration, property.getPath(), `var(${getCustomPropertyVariableName(property)})`);
   });
 
