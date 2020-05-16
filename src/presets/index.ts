ee93716e3901b804658509f1e5f967b18044c148
Enzo Innocenzi <enzo@innocenzi.dev>
2020-05-16 03:01:14 +0200

    feat: Added Tailwind CSS vanilla preset

--- src/presets/index.ts
+++ src/presets/index.ts
@@ -1,4 +1,5 @@
 import { Nord, NordVanilla } from './nord';
+import { TailwindVanilla } from './tailwind';
 
 /**
  * The list of presets available.
@@ -6,6 +7,7 @@ import { Nord, NordVanilla } from './nord';
 export const presets = {
   nord: Nord,
   'nord-vanilla': NordVanilla,
+  'tailwind-vanilla': TailwindVanilla,
 };
 
 /**
@@ -18,3 +20,9 @@ export type Preset = keyof typeof presets;
  * https://www.nordtheme.com/docs/colors-and-palettes
  */
 export { NordPalette } from './nord';
+
+/**
+ * The Tailwind CSS color palette.
+ * https://tailwindcss.com/docs/customizing-colors#default-color-palette
+ */
+export { TailwindPalette } from './tailwind';
